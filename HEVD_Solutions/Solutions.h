403f6b91046b50c3b4a301d5748552015ce0edab
yuval <yuvalyohanan3@gmail.com>
2020-09-12 17:46:36 +0300

    added .h for solution

--- HEVD_Solutions/Solutions.h
+++ HEVD_Solutions/Solutions.h
@@ -20,28 +20,7 @@ class Solutions {
 		NTSTATUS TriggerUninitializedHeapVariable();
 		NTSTATUS TriggerDoubleFetch();
 
-		NTSTATUS TriggerExploit(int choice) {
-			switch (choice) {
-			case 1:
-				return this->TriggerStackBufferOverflow();
-			case 2:
-				return this->TriggerIntegerOverflow();
-			case 3:
-				return this->TriggerWriteWhatWhere();
-			case 4:
-				return this->TriggerNullPointerDereference();
-			case 5:
-				return this->TriggerUAF();
-			case 6: 
-				return this->TriggerNonPagedPoolOverflow();
-			case 7:
-				return this->TriggerUninitializedStackVariable();
-			case 8:
-				return this->TriggerUninitializedHeapVariable();
-			case 9:
-				return this->TriggerDoubleFetch();
-			}
-		}
+		NTSTATUS TriggerExploit(int choice);
 
 	private:
 		HANDLE _hDeviceHandle;
